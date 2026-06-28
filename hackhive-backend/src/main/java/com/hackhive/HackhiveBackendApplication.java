e64b8305969e0f6322b158e26acfb364ab2d96cb
MaheshSomu <somumahesh8886@gmail.com>
2026-06-28 15:59:41 +0530

    Initial project setup and architecture

---
+++ hackhive-backend/src/main/java/com/hackhive/HackhiveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hackhive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackhiveBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackhiveBackendApplication.class, args);
+	}
+
+}
---
+++ hackhive-backend/src/test/java/com/hackhive/HackhiveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackhive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackhiveBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
