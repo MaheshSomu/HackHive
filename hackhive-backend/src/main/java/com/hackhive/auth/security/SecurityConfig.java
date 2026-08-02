ff7ac34cf7ad28da9dfe9ff3759e77d39e4f1e17
MaheshSomu <somumahesh8886@gmail.com>
2026-08-02 11:19:06 +0530

    added cors configuration

--- hackhive-backend/src/main/java/com/hackhive/auth/security/SecurityConfig.java
+++ hackhive-backend/src/main/java/com/hackhive/auth/security/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
             throws Exception {
 
         http
-
+                .cors(cors->{})
                 .csrf(csrf -> csrf.disable())
 
                 .sessionManagement(session ->
